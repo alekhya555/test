62a5481c8f2e95e445502c08a7c28ce6349cb35b
swathi-0901 <43814493+swathi-0901@users.noreply.github.com>
2018-10-15 15:40:25 +0530

    Update simplearraysum.py

--- simplearraysum.py
+++ simplearraysum.py
@@ -1,6 +1,10 @@
-n=int(input())
+# this program is to find the of numbers in an array
+# first it takes number of elements in an array
+# then it takes list of elements in an array
+# finally finds their sum
+n=int(input("enter number of elements "))
 ar=list(map(int, input().strip().split(" ")))
 sum=0
 for i in range(0,n):
 	sum=sum+ar[i]
-print(sum)
+print(sum,"is sum of elements")
